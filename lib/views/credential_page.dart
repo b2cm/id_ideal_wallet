024e9db26a0d66fe8028fa87fb43bb7da7b4835c
Sarah Otto <otto3@hs-mittweida.de>
2022-11-08 14:26:01 +0100

    handle image data urls in credential subject

--- lib/views/credential_page.dart
+++ lib/views/credential_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dart_ssi/credentials.dart';
 import 'package:flutter/material.dart';
 import 'package:id_ideal_wallet/provider/wallet_provider.dart';
@@ -5,6 +7,7 @@ import 'package:id_ideal_wallet/views/credential_detail.dart';
 import 'package:id_ideal_wallet/views/issuer_info.dart';
 import 'package:id_ideal_wallet/views/qr_scanner.dart';
 import 'package:id_wallet_design/id_wallet_design.dart';
+import 'package:json_path/json_path.dart';
 import 'package:provider/provider.dart';
 
 class CredentialPage extends StatelessWidget {
@@ -31,7 +34,8 @@ List<Widget> buildCredSubject(Map<String, dynamic> subject, [String? before]) {
         List<Widget> subs = buildCredSubject(value, key);
         children.addAll(subs);
       } else {
-        children.add(Text('${before != null ? '$before.' : ''}$key: $value'));
+        children.add(Text(
+            '${before != null ? '$before.' : ''}$key: ${(value is String && value.startsWith('data:image')) ? '...' : value}'));
       }
     }
   });
@@ -74,22 +78,55 @@ class CredentialOverview extends StatelessWidget {
   }
 }
 
-class CredentialCard extends StatelessWidget {
+class CredentialCard extends StatefulWidget {
   final VerifiableCredential credential;
 
   const CredentialCard({Key? key, required this.credential}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => CredentialCardState();
+}
+
+class CredentialCardState extends State<CredentialCard> {
+  Image? image;
+
+  @override
+  void initState() {
+    super.initState();
+    searchImage();
+  }
+
+  Future<void> searchImage() async {
+    try {
+      final path = JsonPath(r'$.credentialSubject..[?image]', filters: {
+        'image': (match) =>
+            match.value is String && match.value.startsWith('data:image')
+      });
+
+      var result = path.read(widget.credential.toJson());
+      var dataString = result.first.value as String;
+      var imageData = dataString.split(',').last;
+
+      image = Image.memory(base64Decode(imageData));
+      setState(() {});
+    } catch (e) {
+      print('cant decode image: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => CredentialDetailView(credential: credential))),
+          builder: (context) =>
+              CredentialDetailView(credential: widget.credential))),
       child: IdCard(
-          cardTitle: credential.type
+          subjectImage: image?.image,
+          cardTitle: widget.credential.type
               .firstWhere((element) => element != 'VerifiableCredential'),
           subjectName:
-              '${credential.credentialSubject['givenName'] ?? ''} ${credential.credentialSubject['familyName'] ?? ''}',
-          bottomLeftText: IssuerInfo(issuer: credential.issuer),
+              '${widget.credential.credentialSubject['givenName'] ?? ''} ${widget.credential.credentialSubject['familyName'] ?? ''}',
+          bottomLeftText: IssuerInfo(issuer: widget.credential.issuer),
           bottomRightText: ''),
     );
   }
