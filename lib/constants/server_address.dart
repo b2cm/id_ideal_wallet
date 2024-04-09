47c07ab957ded894153fda243d1c59620b5bafe5
Sarah Otto <otto3@hs-mittweida.de>
2024-04-09 15:10:13 +0200

    fix small appearance bugs in payment overview when clicked on issued credential

--- lib/constants/server_address.dart
+++ lib/constants/server_address.dart
@@ -15,7 +15,7 @@ var applicationEndpoint =
     'https://hidy.app/walletcontext/apps?plattform=${Platform.isIOS ? '1' : '2'}';
 var stylingEndpoint = 'https://hidy.app/walletcontext/layouts';
 var termsVersionEndpoint = 'https://hidy.app/walletcontext/terms';
-String versionNumber = '2.4.8';
+String versionNumber = '2.4.9';
 String baseUrl = 'https://hidy.app';
 bool testBuild = false;
 
