f948fe366ce295d0e97e843cdc6355cd564be5fc
Sarah Otto <otto3@hs-mittweida.de>
2024-10-02 11:33:33 +0200

    redesign new abo screen

--- lib/views/abo_overview.dart
+++ lib/views/abo_overview.dart
@@ -61,141 +61,152 @@ class AboOverviewState extends State<AboOverview>
             child: Consumer<WalletProvider>(builder: (context, wallet, child) {
               return wallet.aboList.isNotEmpty
                   ? SizedBox.expand(
-                      child: Wrap(
-                      alignment: WrapAlignment.spaceEvenly,
-                      children: List.generate(wallet.aboList.length, (index) {
-                        var e = wallet.aboList[index];
-                        return GestureDetector(
-                          onTapDown: (details) {
-                            tapPosition = details.globalPosition;
-                          },
-                          onLongPress: () {
-                            controllers[index].forward();
-                            controllers[index].repeat(reverse: true);
+                      child: SingleChildScrollView(
+                        child: Wrap(
+                          alignment: WrapAlignment.spaceEvenly,
+                          children:
+                              List.generate(wallet.aboList.length, (index) {
+                            var e = wallet.aboList[index];
+                            return GestureDetector(
+                              onTapDown: (details) {
+                                tapPosition = details.globalPosition;
+                              },
+                              onLongPress: () {
+                                controllers[index].forward();
+                                controllers[index].repeat(reverse: true);
 
-                            final RenderBox overlay = Overlay.of(context)
-                                .context
-                                .findRenderObject() as RenderBox;
+                                final RenderBox overlay = Overlay.of(context)
+                                    .context
+                                    .findRenderObject() as RenderBox;
 
-                            final RenderBox card = cardKeys[index]
-                                .currentContext!
-                                .findRenderObject() as RenderBox;
+                                final RenderBox card = cardKeys[index]
+                                    .currentContext!
+                                    .findRenderObject() as RenderBox;
 
-                            final RelativeRect position = RelativeRect.fromRect(
-                              Rect.fromPoints(
-                                card.localToGlobal(
-                                  const Offset(0, -50),
-                                ),
-                                card.localToGlobal(
-                                  card.size.topLeft(Offset.zero) +
-                                      const Offset(55, 0),
-                                ),
-                              ),
-                              Offset.zero & overlay.size,
-                            );
-
-                            showMenu(
-                              color: Colors.white,
-                              surfaceTintColor: Colors.white,
-                              context: context,
-                              position: position,
-                              constraints: const BoxConstraints(
-                                  minWidth: 30, minHeight: 30),
-                              items: [
-                                CustomPopupMenuItem(
-                                  onTap: () {
-                                    deleteSelected = true;
-                                  },
-                                  color: Colors.white,
-                                  child: const SizedBox(
-                                    width: 30,
-                                    height: 30,
-                                    child: Icon(
-                                      Icons.delete,
+                                final RelativeRect position =
+                                    RelativeRect.fromRect(
+                                  Rect.fromPoints(
+                                    card.localToGlobal(
+                                      const Offset(0, -50),
+                                    ),
+                                    card.localToGlobal(
+                                      card.size.topLeft(Offset.zero) +
+                                          const Offset(55, 0),
                                     ),
                                   ),
-                                )
-                              ],
-                            ).then((value) async {
-                              if (deleteSelected) {
-                                await showDialog(
-                                  context: context,
-                                  builder: (context) => AlertDialog(
-                                    title: Text(
-                                        AppLocalizations.of(context)!.delete),
-                                    content: Card(
-                                        child: Text(
-                                            AppLocalizations.of(context)!
-                                                .deletionNoteApp)),
-                                    actions: [
-                                      TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: Text(
-                                              AppLocalizations.of(context)!
-                                                  .cancel)),
-                                      TextButton(
-                                          onPressed: () async {
-                                            wallet.deleteAbo(index);
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: Text(
-                                              AppLocalizations.of(context)!
-                                                  .delete))
-                                    ],
-                                  ),
+                                  Offset.zero & overlay.size,
                                 );
-                              }
-                              controllers[index].reset();
-                              deleteSelected = false;
-                            });
-                          },
-                          onTap: () {
-                            Provider.of<NavigationProvider>(context,
-                                    listen: false)
-                                .changePage([NavigationPage.webView],
-                                    webViewUrl: e['url'].toString().replaceAll(
-                                        'wid=', 'wid=${wallet.lndwId}'));
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.4,
-                              child: SlideTransition(
-                                position: animations[index],
-                                child: ContextCredentialCard(
-                                  key: cardKeys[index],
-                                  borderWidth: 1,
-                                  edgeRadius: 10,
-                                  cardTitle: '',
-                                  backgroundImage: e
-                                              .containsKey('mainbgimage') &&
-                                          e['mainbgimage']!.isNotEmpty
-                                      ? Image.network(e['mainbgimage']!).image
-                                      : null,
-                                  backgroundColor: Colors.green.shade300,
-                                  cardTitleColor:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                  subjectName:
-                                      e['name'] != null && e['name']!.isNotEmpty
+
+                                showMenu(
+                                  color: Colors.white,
+                                  surfaceTintColor: Colors.white,
+                                  context: context,
+                                  position: position,
+                                  constraints: const BoxConstraints(
+                                      minWidth: 30, minHeight: 30),
+                                  items: [
+                                    CustomPopupMenuItem(
+                                      onTap: () {
+                                        deleteSelected = true;
+                                      },
+                                      color: Colors.white,
+                                      child: const SizedBox(
+                                        width: 30,
+                                        height: 30,
+                                        child: Icon(
+                                          Icons.delete,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ).then((value) async {
+                                  if (deleteSelected) {
+                                    await showDialog(
+                                      context: context,
+                                      builder: (context) => AlertDialog(
+                                        title: Text(
+                                            AppLocalizations.of(context)!
+                                                .delete),
+                                        content: Card(
+                                            child: Text(
+                                                AppLocalizations.of(context)!
+                                                    .deletionNoteApp)),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text(
+                                                  AppLocalizations.of(context)!
+                                                      .cancel)),
+                                          TextButton(
+                                              onPressed: () async {
+                                                wallet.deleteAbo(index);
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text(
+                                                  AppLocalizations.of(context)!
+                                                      .delete))
+                                        ],
+                                      ),
+                                    );
+                                  }
+                                  controllers[index].reset();
+                                  deleteSelected = false;
+                                });
+                              },
+                              onTap: () {
+                                Provider.of<NavigationProvider>(context,
+                                        listen: false)
+                                    .changePage([NavigationPage.webView],
+                                        webViewUrl: e['url']
+                                            .toString()
+                                            .replaceAll('wid=',
+                                                'wid=${wallet.lndwId}'));
+                              },
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5),
+                                child: SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                  child: SlideTransition(
+                                    position: animations[index],
+                                    child: ContextCredentialCard(
+                                      key: cardKeys[index],
+                                      borderWidth: 1,
+                                      edgeRadius: 10,
+                                      cardTitle: '',
+                                      backgroundImage:
+                                          e.containsKey('mainbgimage') &&
+                                                  e['mainbgimage']!.isNotEmpty
+                                              ? Image.network(e['mainbgimage']!)
+                                                  .image
+                                              : null,
+                                      backgroundColor: Colors.green.shade300,
+                                      cardTitleColor: const Color.fromARGB(
+                                          255, 255, 255, 255),
+                                      subjectName: e['name'] != null &&
+                                              e['name']!.isNotEmpty
                                           ? e['name']!
                                           : e['url'] != null
                                               ? e['url']!
                                               : '',
-                                  bottomLeftText: const SizedBox(
-                                    width: 0,
-                                  ),
-                                  bottomRightText: const SizedBox(
-                                    width: 0,
+                                      bottomLeftText: const SizedBox(
+                                        width: 0,
+                                      ),
+                                      bottomRightText: const SizedBox(
+                                        width: 0,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    ))
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    )
                   : Center(
                       child: Text(AppLocalizations.of(context)!.noAppNote));
             }),
--- lib/views/search_new_abo.dart
+++ lib/views/search_new_abo.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:http/http.dart';
+import 'package:id_ideal_wallet/basicUi/standard/id_card.dart';
 import 'package:id_ideal_wallet/basicUi/standard/styled_scaffold_title.dart';
+import 'package:id_ideal_wallet/constants/navigation_pages.dart';
 import 'package:id_ideal_wallet/constants/server_address.dart';
 import 'package:id_ideal_wallet/provider/navigation_provider.dart';
 import 'package:id_ideal_wallet/provider/wallet_provider.dart';
@@ -19,7 +21,6 @@ class SearchNewAbo extends StatefulWidget {
 class SearchNewAboState extends State<SearchNewAbo> {
   bool searching = true;
   List<Map<String, dynamic>> toShow = [];
-  List<bool> checked = [];
 
   @override
   void initState() {
@@ -43,14 +44,13 @@ class SearchNewAboState extends State<SearchNewAbo> {
     }
 
     toShow = [];
-    checked = [];
+
     if (available.isNotEmpty) {
       for (var entry in available) {
         var asUri = Uri.parse(entry['url']);
         var toCheck = '${asUri.scheme}://${asUri.host}${asUri.path}';
         if (!inAbo.contains(toCheck)) {
           toShow.add(entry);
-          checked.add(false);
         }
       }
     }
@@ -64,62 +64,57 @@ class SearchNewAboState extends State<SearchNewAbo> {
   Widget build(BuildContext context) {
     return StyledScaffoldTitle(
       title: AppLocalizations.of(context)!.newAppTitle,
-      fab: !searching && toShow.isNotEmpty
-          ? FloatingActionButton.extended(
-              onPressed: () {
-                var wallet =
-                    Provider.of<WalletProvider>(context, listen: false);
-                for (int i = 0; i < checked.length; i++) {
-                  if (checked[i]) {
-                    var entry = toShow[i];
-                    wallet.addAbo(
-                        entry['url'], entry['mainbgimg'], entry['name']);
-                  }
-                }
-                Provider.of<NavigationProvider>(context, listen: false)
-                    .goBack();
-              },
-              label: Text(AppLocalizations.of(context)!.add),
-            )
-          : null,
       child: searching
           ? const Center(child: CircularProgressIndicator())
           : toShow.isEmpty
               ? Center(child: Text(AppLocalizations.of(context)!.newAppNote))
-              : ListView.separated(
-                  padding: const EdgeInsets.only(bottom: bottomPadding),
-                  itemCount: toShow.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      trailing: Checkbox(
-                        value: checked[index],
-                        onChanged: (bool? value) {
-                          if (value != null) {
-                            setState(() {
-                              checked[index] = value;
-                            });
-                          }
-                        },
-                      ),
-                      leading: Container(
-                          width: 100,
-                          decoration: BoxDecoration(
-                              border: Border.all(),
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                  scale: 0.2,
-                                  fit: BoxFit.cover,
-                                  image:
-                                      Image.network(toShow[index]['mainbgimg'])
-                                          .image))),
-                      title: Text(toShow[index]['name'] ?? ''),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(
-                      height: 7,
-                    );
-                  },
+              : SizedBox.expand(
+                  child: SingleChildScrollView(
+                    child: Wrap(
+                      alignment: WrapAlignment.spaceEvenly,
+                      children: List.generate(toShow.length, (index) {
+                        var e = toShow[index];
+                        return InkWell(
+                          onTap: () {
+                            Provider.of<NavigationProvider>(context,
+                                    listen: false)
+                                .changePage([NavigationPage.webView],
+                                    webViewUrl: e['url']);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              child: ContextCredentialCard(
+                                borderWidth: 1,
+                                edgeRadius: 10,
+                                cardTitle: '',
+                                backgroundImage: e.containsKey('mainbgimg') &&
+                                        e['mainbgimg']!.isNotEmpty
+                                    ? Image.network(e['mainbgimg']!).image
+                                    : null,
+                                backgroundColor: Colors.green.shade300,
+                                cardTitleColor:
+                                    const Color.fromARGB(255, 255, 255, 255),
+                                subjectName:
+                                    e['name'] != null && e['name']!.isNotEmpty
+                                        ? e['name']!
+                                        : e['url'] != null
+                                            ? e['url']!
+                                            : '',
+                                bottomLeftText: const SizedBox(
+                                  width: 0,
+                                ),
+                                bottomRightText: const SizedBox(
+                                  width: 0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
                 ),
     );
   }
